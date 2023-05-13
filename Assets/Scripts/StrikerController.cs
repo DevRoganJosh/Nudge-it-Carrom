a64e9fe21a7f234a849988189693a960aa5566ea
Samay28 <imsamaystar@gmail.com>
2023-05-13 18:05:57 +0530

    day2

--- Assets/Scripts/StrikerController.cs
+++ Assets/Scripts/StrikerController.cs
@@ -6,13 +6,18 @@ public class StrikerController : MonoBehaviour
 {
     Rigidbody2D rb;
     private Vector3 initialPosition;
+    
+    private Vector3 LockedPosition;
     private Vector3 mousePosition;
     private bool isDraggingX = false;
     bool LockedX = false;
     bool isCharging = false;
     float boundary1;
     float boundary2;
-
+    float chargeClampY;
+    float ChargeClampX1;
+    float ChargeClampX2;
+    RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +26,11 @@ public class StrikerController : MonoBehaviour
         initialPosition = transform.position;
         boundary1 = -1.53f;
         boundary2 = 1.53f;
+        chargeClampY = -2.6f;
+        ChargeClampX1 = -0.42f;
+        ChargeClampX2 = 0.42f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isDraggingX)
@@ -32,12 +39,19 @@ public class StrikerController : MonoBehaviour
             float newX = Mathf.Clamp(mousePosition.x, boundary1, boundary2);
             transform.position = new Vector3(newX, initialPosition.y, initialPosition.z);
         }
-
+        if (isCharging)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float CX = Mathf.Clamp(mousePosition.x, LockedPosition.x - 0.42f, LockedPosition.x + 0.42f);
+            float CY = Mathf.Clamp(mousePosition.y, chargeClampY, -2.17f);
+            transform.position = new Vector3(CX, CY, initialPosition.z);
+        }
     }
 
     private void OnMouseDown()
     {
         if (!LockedX)
+        {
             if (!isDraggingX)
             {
                 isDraggingX = true;
@@ -45,7 +59,13 @@ public class StrikerController : MonoBehaviour
             else
             {
                 isDraggingX = false;
+                LockedPosition = transform.position;
             }
+        }
+        else
+        {   
+            isCharging = true;
+        }
     }
 
     public void onClickLock()
@@ -53,7 +73,7 @@ public class StrikerController : MonoBehaviour
         if (!LockedX)
         {
             LockedX = true;
-            isCharging = true;
+            
         }
         else
         {
