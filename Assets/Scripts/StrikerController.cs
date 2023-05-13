f9f245db3104f3466fc5b4013d80223ce0d7505f
Samay28 <imsamaystar@gmail.com>
2023-05-13 09:51:46 +0530

    day 2

---
+++ Assets/Scripts/StrikerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private Vector3 initialPosition;
+    private Vector3 mousePosition;
+    private bool isDraggingX = false;
+    bool LockedX = false;
+    float boundary1;
+    float boundary2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+        boundary1 = -1.53f;
+        boundary2 = 1.53f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDraggingX)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float newX = Mathf.Clamp(mousePosition.x, boundary1, boundary2);
+            transform.position = new Vector3(newX, initialPosition.y, initialPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {   if(!LockedX)
+        if(!isDraggingX)
+        {
+        isDraggingX = true;
+        }
+        else
+        {
+            isDraggingX = false;
+        }
+    }
+
+    public void onClickLock()
+    {   if(!LockedX)
+    {
+        LockedX = true;
+    }
+        else
+        {
+            LockedX = false;
+        }
+    }
+
+    
+}
